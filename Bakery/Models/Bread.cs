92cc6c641c93738eb0154a8153d8fba0ac05ab93
blacchoy@gmail.com <blacchoy@gmail.com>
2020-07-20 06:49:52 -0700

    add new logic to bread.cs

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,12 +1,47 @@
+using System;
+using System.Collections.Generic;
+
 namespace Bakery.Models
 {
   public class Bread
+{
+  static void Main()
   {
-    public string BreadName { get; }
+    string[] breadList = {"red bean", "chocolate", "banana", "rye", "brown", "white", "pink fluffy"};
+    int[] breadListPrices = { 3, 6, 4, 2, 4, 4};
 
-    public Bread(string breadName)
+    Console.WriteLine("Bread Menu");
+    foreach (string breadItem in myBreadList)
     {
-      BreadName = breadName;
+      Console.WriteLine(breadItem);
     }
+
+    int total = 0;
+    foreach (int price in breadListPrices)
+    {
+      total = total + price;
+    }
+
+    Console.WriteLine("Your total bread cost is" + total);
+  }
 }
-}
+//   public class Bread
+//   {
+//     public string BreadName { get; set;}
+//     public int BreadPrice { get; set; }
+//     public int BreadAmount { get; set; }
+//     public int FinalCost { get; set; }
+
+//     public Bread(string breadName, int breadPrice, int breadAmount, int finalCost )
+//     {
+//       BreadName = breadName;
+//     }
+
+//     public void BreadCost()
+//     {
+//       FinalCost = BreadAmount * 5;
+//     }
+
+    
+// }
+// }
