559f85548c6527e5c83925781e1ed57224f58d70
blacchoy@gmail.com <blacchoy@gmail.com>
2020-07-20 07:05:22 -0700

    add logic for dictonary to bread.cs

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -5,43 +5,24 @@ namespace Bakery.Models
 {
   public class Bread
 {
-  static void Main()
-  {
-    string[] breadList = {"red bean", "chocolate", "banana", "rye", "brown", "white", "pink fluffy"};
-    int[] breadListPrices = { 3, 6, 4, 2, 4, 4};
-
-    Console.WriteLine("Bread Menu");
-    foreach (string breadItem in myBreadList)
+  public string Name {get; set;}
+  
+  public Bread(string word)
     {
-      Console.WriteLine(breadItem);
+      Name = name;
     }
-
-    int total = 0;
-    foreach (int price in breadListPrices)
+    public bool IsBread(string word)
     {
-      total = total + price;
+      return true;
     }
-
-    Console.WriteLine("Your total bread cost is" + total);
-  }
+    public Dictionary<char, int> AssignValues()
+    {
+      Dictionary<char, int> bread = new Dictionary<string, int> 
+      {{'Red Bean', 3},
+      {'White', 2},
+      {'Rye', 4}};
+      return bread;
+    }
 }
-//   public class Bread
-//   {
-//     public string BreadName { get; set;}
-//     public int BreadPrice { get; set; }
-//     public int BreadAmount { get; set; }
-//     public int FinalCost { get; set; }
+}
 
-//     public Bread(string breadName, int breadPrice, int breadAmount, int finalCost )
-//     {
-//       BreadName = breadName;
-//     }
-
-//     public void BreadCost()
-//     {
-//       FinalCost = BreadAmount * 5;
-//     }
-
-    
-// }
-// }
