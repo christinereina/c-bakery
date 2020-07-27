6d7cb6d82fa355ce425d160a3ed9016c77b62c0a
blacchoy@gmail.com <blacchoy@gmail.com>
2020-07-27 07:43:49 -0700

    pass 1st Pasty test

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -1,26 +1,18 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using Bakery.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
 
-// namespace Bakery.Tests
-// {
-//   [TestClass]
-//   public class PastryTests
-//   {
-//     [TestMethod]
-//     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
-//     {
-//       Pastry newPastry = new Pastry("Chocolate Croissant");
-//       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
-//     }
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
 
-//     [TestMethod]
-//     public void GetPastryName_ReturnsPastryName_String()
-//     {
-//       string pastryName = "Chocolate Croissant";
-//       Pastry newPastry = new Pastry(pastryName);
-//       string choice = newPastry.PastryName;
-//       Assert.AreEqual(pastryName, choice);
-//     }
+    [TestMethod]
+   public void TestConstructor_CreateInstanceOfPastry_Pastry()
+    {
+      Bread newBread = new Bread(1);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
 
-//   }
-// }
+  }
+}
